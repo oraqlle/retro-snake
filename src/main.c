a6217b64a5a24a324ce5657fbd40b799a60111a9
oraqlle <oraqlle@proton.me>
2026-06-20 19:37:24 +1000

    wip: Game loop

--- src/main.c
+++ src/main.c
@@ -1,25 +1,44 @@
 #include <raylib.h>
 
+#include <stdio.h>
+
 const int TARGET_FPS = 60;
 
-const int screen_width = 800;
-const int screen_height = 450;
+const int screen_width = 1000;
+const int screen_height = 1000;
+const Color background_colour = { .r = 245, .g = 245, .b = 245, .a = 255 };
+
+typedef struct {
+    int x;
+    int y;
+} Vector2i;
+
+void input(void) {}
+
+void update(float delta) { delta = delta * 2; }
+
+void draw(void) {
+    Rectangle rec1 = { .x = 420, .y = 290, .width = 20, .height = 20 };
+    Rectangle rec2 = { .x = 420, .y = 315, .width = 20, .height = 20 };
+
+    DrawRectangleRec(rec1, BLACK);
+    DrawRectangleRec(rec2, BLACK);
+}
 
 int main(void) {
     InitWindow(screen_width, screen_height, "Snake");
     SetTargetFPS(TARGET_FPS);
 
     while (!WindowShouldClose()) {
+        float delta = GetFrameTime();
+
+        input();
+        update(delta);
+
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-
-        DrawText("Hello World!", 290, 220, 40, GRAY);
-
-        if (IsKeyPressed(KEY_ESCAPE)) {
-            EndDrawing();
-            break;
-        }
-
+        ClearBackground(background_colour);
+        DrawFPS(0, 0);
+        draw();
         EndDrawing();
     }
 
