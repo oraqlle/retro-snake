933a566d3afb8b4c7a0f90958c45744954c4cb90
oraqlle <oraqlle@proton.me>
2026-06-21 10:28:38 +1000

    mech: Snake position updating

--- src/main.c
+++ src/main.c
@@ -3,26 +3,26 @@
 #include <stdio.h>
 
 // Global Constants
-#define CELL_W 30 //< px
-#define CELL_H 30 //< px
+#define CELL_W 30  //< px
+#define CELL_H 30  //< px
 #define CELL_GAP 2 //< px
 
 #define GRID_W 30 //< cell
 #define GRID_H 30 //< cell
-#define NUM_CELLS GRID_W * GRID_H
+#define NUM_CELLS GRID_W *GRID_H
 
 const int TARGET_FPS = 60;
 
-const int SIDE_PANEL_W = 500; //< px
+const int SIDE_PANEL_W = 500;               //< px
 const int SIDE_PANEL_H = (GRID_H * CELL_H); //< px
 
 const int BOARD_W = GRID_W * CELL_W;
 const int BOARD_H = SIDE_PANEL_H;
 
 const int SCREEN_W = BOARD_W + SIDE_PANEL_W; //< px
-const int SCREEN_H = SIDE_PANEL_H; //< px
+const int SCREEN_H = SIDE_PANEL_H;           //< px
 
-const Color BACKGROUND_COLOUR = { .r = 172, .g = 182, .b = 7, .a = 255 };
+const Color BACKGROUND_COLOUR = {.r = 172, .g = 182, .b = 7, .a = 255};
 
 const float SNAKE_CELL_W = CELL_W - CELL_GAP - CELL_GAP;
 const float SNAKE_CELL_H = CELL_H - CELL_GAP - CELL_GAP;
@@ -39,12 +39,7 @@ typedef struct {
     unsigned y;
 } Vector2u;
 
-typedef enum {
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT
-} Direction;
+typedef enum { UP, DOWN, LEFT, RIGHT } Direction;
 
 typedef struct {
     Vector2i positions[NUM_CELLS];
@@ -80,24 +75,57 @@ void draw_snake(Snake *snake) {
     for (unsigned idx = 0; idx < snake->length; idx++) {
         float screen_x = (float)(snake->positions[idx].x * CELL_W) + SNAKE_CELL_OFFSET;
         float screen_y = (float)(snake->positions[idx].y * CELL_H) + SNAKE_CELL_OFFSET;
-        Rectangle rect = { .x = screen_x, .y = screen_y, .width = SNAKE_CELL_W, .height = SNAKE_CELL_H };
+        Rectangle rect = {
+            .x = screen_x, .y = screen_y, .width = SNAKE_CELL_W, .height = SNAKE_CELL_H};
         DrawRectangleRec(rect, BLACK);
     }
 }
 
+static float time = 0.0f;
+void update_snake(Snake *snake, float delta) {
+    time += delta;
+
+    if (time >= 0.5f) {
+
+        time = 0.0f;
+
+        for (unsigned idx = snake->length - 1; idx > 0; idx--) {
+            Vector2i nxt_pos = snake->positions[idx - 1];
+            snake->positions[idx].x = nxt_pos.x;
+            snake->positions[idx].y = nxt_pos.y;
+        }
+
+        switch (snake->dir) {
+            case UP:
+                snake->positions[0].y -= 1;
+                break;
+            case DOWN:
+                snake->positions[0].y += 1;
+                break;
+            case LEFT:
+                snake->positions[0].x -= 1;
+                break;
+            case RIGHT:
+                snake->positions[0].x += 1;
+                break;
+        }
+    }
+}
+
 void draw_game_panel(void) {
-    Rectangle board = { .x = 5, .y = 5, .width = BOARD_W - 10, .height = BOARD_H - 10 };
+    Rectangle board = {.x = 5, .y = 5, .width = BOARD_W - 10, .height = BOARD_H - 10};
     DrawRectangleLinesEx(board, 10, BLACK);
 }
 
 void draw_side_panel(void) {
-    Rectangle board = { .x = BOARD_W, .y = 5, .width = SIDE_PANEL_W - 5, .height = SIDE_PANEL_H - 10 };
+    Rectangle board = {
+        .x = BOARD_W, .y = 5, .width = SIDE_PANEL_W - 5, .height = SIDE_PANEL_H - 10};
     DrawRectangleLinesEx(board, 10, BLACK);
 }
 
 void input(void) {}
 
-void update(float delta) { delta = delta * 2; }
+void update(float delta) { update_snake(&snake, delta); }
 
 void draw(void) {
     draw_game_panel();
